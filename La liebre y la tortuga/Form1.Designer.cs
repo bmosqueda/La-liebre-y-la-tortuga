37c8d58654ad3900e5fe5a5dfceb688806aab864
Brandon Alejandro Mosqueda González <brandonMG_1698@hotmail.com>
2018-03-20 11:45:04 -0600

    primer commit, terminado

---
+++ La liebre y la tortuga/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace La_liebre_y_la_tortuga
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCarrera = new System.Windows.Forms.RichTextBox();
+            this.btnComenzar = new System.Windows.Forms.Button();
+            this.lblGanador = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCarrera
+            // 
+            this.txtCarrera.Location = new System.Drawing.Point(66, 63);
+            this.txtCarrera.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCarrera.Name = "txtCarrera";
+            this.txtCarrera.Size = new System.Drawing.Size(342, 415);
+            this.txtCarrera.TabIndex = 0;
+            this.txtCarrera.Text = "";
+            // 
+            // btnComenzar
+            // 
+            this.btnComenzar.Location = new System.Drawing.Point(143, 14);
+            this.btnComenzar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnComenzar.Name = "btnComenzar";
+            this.btnComenzar.Size = new System.Drawing.Size(206, 35);
+            this.btnComenzar.TabIndex = 1;
+            this.btnComenzar.Text = "¡Comenzar carrera!";
+            this.btnComenzar.UseVisualStyleBackColor = true;
+            this.btnComenzar.Click += new System.EventHandler(this.btnComenzar_Click);
+            // 
+            // lblGanador
+            // 
+            this.lblGanador.AutoSize = true;
+            this.lblGanador.Location = new System.Drawing.Point(62, 493);
+            this.lblGanador.Name = "lblGanador";
+            this.lblGanador.Size = new System.Drawing.Size(76, 20);
+            this.lblGanador.TabIndex = 2;
+            this.lblGanador.Text = "Ganador:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 533);
+            this.Controls.Add(this.lblGanador);
+            this.Controls.Add(this.btnComenzar);
+            this.Controls.Add(this.txtCarrera);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "La liebre y la tortuga";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtCarrera;
+        private System.Windows.Forms.Button btnComenzar;
+        private System.Windows.Forms.Label lblGanador;
+    }
+}
+
---
+++ La liebre y la tortuga/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace La_liebre_y_la_tortuga
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public abstract class Corredor
+        {
+            public int Posicion { get { return posicion; } }
+            private int posicion;
+            protected static Random aleatorio;
+
+            public Corredor()
+            {
+                posicion = 0;
+                aleatorio = new Random();
+            }
+
+            protected abstract int getAvance();
+
+            public void correr()
+            {
+                posicion += getAvance();
+                //Si se resbala muchas veces no puede caer menos del inicio, 0
+                if (posicion < 0)
+                    posicion = 0;
+            }
+        }
+
+        public class Tortuga : Corredor
+        {
+            protected override int getAvance()
+            {
+                int avance = aleatorio.Next(1, 11); 
+                //rápido 50%    +3 pasos
+                //resbalón 20%  -6 pasos
+                //lento 30%     +1 paso
+                switch(avance)
+                {
+                    case 1: case 2: case 3://lento
+                        return 1;
+                    case 4: case 5://rebalón
+                        return -6;
+                    default://rápido
+                        return 3;
+                }
+            }
+
+            public override string ToString()
+            {
+                return "Toruga está en la posición " + this.Posicion;
+            }
+        }
+
+        public class Liebre : Corredor
+        {
+            protected override int getAvance()
+            {
+                int avance = aleatorio.Next(1, 11);
+                //dormir 20%            0 pasos
+                //salto grande 20%      9 pasos
+                //resbalón grande 10%   -12 pasos
+                //salto pequeño 30%     1 paso
+                //resbalón pequeño 20% -2 pasos
+                switch(avance)
+                {
+                    case 1: case 2: case 3:
+                        return 1;//salto pequeño
+                    case 4: case 5:
+                        return 0;//dormir
+                    case 6: case 7:
+                        return 9;//salto grande
+                    case 8: case 9:
+                        return -2;//resbalón pequeño
+                    default:
+                        return -12;//resbalón grande
+                        
+                }
+            }
+
+            public override string ToString()
+            {
+                return "Liebre está en la posición " + this.Posicion;
+            }
+        }
+
+        private void btnComenzar_Click(object sender, EventArgs e)
+        {
+            txtCarrera.Text = "";
+            Tortuga tortuga = new Tortuga();
+            Liebre liebre = new Liebre();
+
+            //La pista mide 80 pasos
+            while(tortuga.Posicion < 80 && liebre.Posicion < 80)
+            {
+                tortuga.correr();
+                liebre.correr();
+
+                txtCarrera.Text += tortuga.ToString() + "\n" + liebre.ToString() + "\n";
+            }
+
+            if (tortuga.Posicion >= 80 && liebre.Posicion >= 80)
+                lblGanador.Text = "Empate";
+            else
+            {
+                lblGanador.Text = "El ganador es " + (tortuga.Posicion > liebre.Posicion ? "Tortuga" : "Liebre");
+            }
+        }
+    }
+}
